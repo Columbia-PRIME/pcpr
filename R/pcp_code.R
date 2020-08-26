9e85b51acb38ce97056ceaf19a6f6520dbb7e046
lizzyagibson <eag2186@cumc.columbia.edu>
2020-08-26 17:07:36 -0400

    Up iterations and add convergence warning.

--- R/pcp_code.R
+++ R/pcp_code.R
@@ -118,6 +118,7 @@ pcp_lod <- function(D, lambda, mu, LOD) {
   n <- ncol(D)
   rho <- 1 # Augmented Lagrangian coefficient (rate)
 
+
   L1 <- matrix(0, m, n)
   L2 <- matrix(0, m, n)
   L3 <- matrix(0, m, n)
@@ -130,16 +131,11 @@ pcp_lod <- function(D, lambda, mu, LOD) {
   Z3 <- matrix(0, m, n)
 
   # Max iteration
-  MAX_ITER <- 1000
+  MAX_ITER <- 10000
 
   # Convergence Thresholds
   LOSS_THRESH <- 1e-5
   SAME_THRESH <- 1e-4
-
-  # if (is.vector(LOD)) {
-  #   tf = ifelse(D < 0, TRUE, FALSE)
-  #   LOD = t(t(tf) * LOD)
-  # }
 
   if (is.vector(LOD)) {
     empty = matrix(1, nrow = nrow(D), ncol = ncol(D))
@@ -165,7 +161,7 @@ pcp_lod <- function(D, lambda, mu, LOD) {
     L2_opt3 <- ((mu*rho*LOD + (((mu + rho)*Z1) - (mu*Z3) + ((mu + rho)*rho*L1) - (mu*rho*S1)))) / ((2*mu*rho) + (rho^2))
     L2_opt4 <- (               (mu + rho)*Z1 - mu*Z3 + (mu + rho)*rho*L1 - mu*rho*S1) / (2*mu*rho + rho^2)
 
-    L2_new <- (L2_opt1 * (D >= 0)) +
+    L2 <- (L2_opt1 * (D >= 0)) +
       (L2_opt2 * (((D < 0) & ((L2 + S2) >= 0) & ((L2 + S2) <= LOD)))) +
       (L2_opt3 * (((D < 0) & ((L2 + S2) > LOD)))) +
       (L2_opt4 * (((D < 0) & ((L2 + S2) < 0))))
@@ -180,7 +176,7 @@ pcp_lod <- function(D, lambda, mu, LOD) {
       (S2_opt3 * ((D < 0) & (((L2 + S2) > LOD)))) +
       (S2_opt4 * ((D < 0) & (((L2 + S2) < 0))))
 
-    L2 <- L2_new
+    #L2 <- L2_new
 
     L3 <- pmax(L1 + Z2/rho, 0, na.rm = TRUE)
     # % Non-Negativity constraint!
@@ -199,8 +195,6 @@ pcp_lod <- function(D, lambda, mu, LOD) {
       (rho/2 * (sum((L1-L2)^2) + sum((L1 - L3)^2) + sum((S1 - S2)^2)))
     # % The code block above takes LOD into account.
 
-    print(paste0(i, " Obj: ", loss[i]))
-
     if ((i != 1) &&
         (abs(loss[i-1] - loss[i]) < LOSS_THRESH) &&
         is_same(SAME_THRESH, L1, L2, L3) &&
@@ -208,9 +202,13 @@ pcp_lod <- function(D, lambda, mu, LOD) {
       break} # % Convergence criteria!
   }
 
+  print(paste0("Iteration: ", i, " Obj: ", round(loss[i], 5)))
+
   L <- L3 # (L1 + L2 + L3) / 3
   S <- S1 #(S1 + S2) / 2
   list(L = L, S = S)
+
+  if( i == MAX_ITER) warning('Maximum iterations reached. PCP did not converge.')
 }
 
 ############################################################
@@ -221,110 +219,6 @@ pcp_original <- function(D, lambda, mu) {
   pcp_lod(D, lambda, mu, LOD = 0)
   }
 
-############################################################
-## PCP-LOD version where sparse matrix is non-negative, too
-############################################################
-
-pcp_lod_nnS <- function(D, lambda, mu, LOD) {
-
-  m <- nrow(D)
-  n <- ncol(D)
-  rho <- 1 # Augmented Lagrangian coefficient (rate)
-
-  L1 <- matrix(0, m, n)
-  L2 <- matrix(0, m, n)
-  L3 <- matrix(0, m, n)
-
-  S1 <- matrix(0, m, n)
-  S2 <- matrix(0, m, n)
-  S3 <- matrix(0, m, n) # ADDED
-
-  Z1 <- matrix(0, m, n)
-  Z2 <- matrix(0, m, n)
-  Z3 <- matrix(0, m, n)
-  Z4 <- matrix(0, m, n) # ADDED
-
-  # Max iteration
-  MAX_ITER <- 5000
-
-  # Convergence Thresholds
-  LOSS_THRESH <- 1e-5
-  SAME_THRESH <- 1e-4
-
-  if (is.vector(LOD)) {
-    empty = matrix(1, nrow = nrow(D), ncol = ncol(D))
-    LOD = t(t(empty) * LOD)
-  } # This converts a vector LOD to a matrix, so that it multiplies correctly
-
-  loss <- vector("numeric", MAX_ITER)
-
-  for (i in 1:MAX_ITER) {
-
-    nuc <- prox_nuclear(((L2 + L3 - (Z1 + Z2)/rho)/2), 1/2/rho)
-    L1 <- nuc[[1]]
-    nuclearL1 <- nuc[[2]] #nuclearX
-
-    S1 <- prox_l1(((S2 + S3 - (Z3 + Z4)/rho)/2), lambda/rho) # ADDED
-    #prox_l1(S2 - Z3/rho, lambda/rho)
-
-    L2_opt1 <- (mu*rho*D     + (mu + rho)*Z1 - mu*Z3 + (mu + rho)*rho*L1 - mu*rho*S1) / (2*mu*rho + rho^2)
-    L2_opt2 <- L1 + Z1/rho
-    L2_opt3 <- ((mu*rho*LOD + (((mu + rho)*Z1) - (mu*Z3) + ((mu + rho)*rho*L1) - (mu*rho*S1)))) / ((2*mu*rho) + (rho^2))
-    L2_opt4 <- (               (mu + rho)*Z1 - mu*Z3 + (mu + rho)*rho*L1 - mu*rho*S1) / (2*mu*rho + rho^2)
-
-    L2 <- (L2_opt1 * (D >= 0)) +
-      (L2_opt2 * ((D < 0) & (((L2 + S2) >= 0) & ((L2 + S2) <= LOD)))) +
-      (L2_opt3 * ((D < 0) & (((L2 + S2) > LOD)))) +
-      (L2_opt4 * ((D < 0) & (((L2 + S2) < 0))))
-
-    S2_opt1 <- (mu*rho*D     + (mu + rho)*Z3 - (mu*Z1) + (mu + rho)*rho*S1 - mu*rho*L1) / (2*mu*rho + rho^2)
-    S2_opt2 <- S1 + (Z3/rho)
-    S2_opt3 <- (((mu*rho*LOD) + (((mu + rho)*Z3) - (mu*Z1) + ((mu + rho)*rho*S1) - (mu*rho*L1)))) / ((2*mu*rho) + (rho^2))
-    S2_opt4 <- (               (mu + rho)*Z3 - (mu*Z1) + (mu + rho)*rho*S1 - mu*rho*L1) / (2*mu*rho + rho^2)
-
-    S2 <- (S2_opt1 * (D >= 0)) +
-      (S2_opt2 * (((D < 0) & ((L2 + S2) >= 0) & ((L2 + S2) <= LOD)))) +
-      (S2_opt3 * (((D < 0) & ((L2 + S2) > LOD)))) +
-      (S2_opt4 * (((D < 0) & ((L2 + S2) < 0))))
-
-    L3 <- pmax(L1 + Z2/rho, 0, na.rm = TRUE)
-    # % Non-Negativity constraint!
-
-    ## ADDED
-    S3 <- pmax(S1 + Z4/rho, 0, na.rm = TRUE)
-
-    Z1 <- Z1 + rho*(L1 - L2)
-    Z2 <- Z2 + rho*(L1 - L3)
-    Z3 <- Z3 + rho*(S1 - S2)
-    Z4 <- Z4 + rho*(S1 - S3) # ADDED
-    # % Z accumulate differnces between L and L and between S and S
-
-    loss[i] <- nuclearL1 +
-      (lambda*sum(abs(S1))) +
-      (mu*loss_lod((L2 + S2), D, LOD)) +
-      sum(Z1*(L1 - L2)) +
-      sum(Z2*(L1 - L3)) +
-      sum(Z3*(S1 - S2)) +
-      sum(Z4*(S1 - S3)) + # ADDED
-      (rho/2 * (sum((L1-L2)^2) + sum((L1 - L3)^2) + sum((S1 - S2)^2)) + sum((S1 - S3)^2)) # ADDED
-    # % The code block above takes LOD into account.
-
-    print(paste0(i, " Obj: ", loss[i]))
-
-    if ((i != 1) &&
-        (abs(loss[i-1] - loss[i]) < LOSS_THRESH) &&
-        is_same(SAME_THRESH, L1, L2, L3) &&
-        is_same(SAME_THRESH, S1, S2, S3)) { # ADDED
-      break} # % Convergence criteria!
-  }
-
-  L <- L3 #(L1 + L2 + L3) / 3
-  S <- S3 #(S1 + S2 + S3) / 3
-  list(L = L, S = S)
-}
-
-############################################################
-############################################################
 
 
 
